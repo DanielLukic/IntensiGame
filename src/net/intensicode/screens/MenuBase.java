9d6be998db6d182f42f3c5021ac135f8f8d1045b
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-08 16:51:17 +0100

    Added new beforeInitEverytime/afterInitEverytime API to MenuBase and changed layout to 2x font height spacing and vertically centered positioning.

--- src/net/intensicode/screens/MenuBase.java
+++ src/net/intensicode/screens/MenuBase.java
@@ -6,16 +6,38 @@ import net.intensicode.util.*;
 
 public abstract class MenuBase extends MultiScreen implements TouchableHandler
     {
-    public final DynamicArray menuEntries = new DynamicArray( 5, 5 );
+    public final DynamicArray entries = new DynamicArray();
 
-    public final FontGenerator menuFontGen;
+    public final FontGenerator font;
 
-    public int selectedEntry;
+    public int selectedEntryIndex;
+
+    public int yOffset;
+
+    public int ySpacingFixed = FixedMath.FIXED_1 * 2;
 
 
     public MenuBase( final FontGenerator aMenuFont )
         {
-        menuFontGen = aMenuFont;
+        font = aMenuFont;
+        yOffset = aMenuFont.charHeight();
+        }
+
+    public final void setOffsetToVerticallyCentered()
+        {
+        yOffset = ( screen().height() - ( entries.size - 1 ) * FixedMath.toInt( font.charHeight() * ySpacingFixed ) ) / 2;
+        }
+
+    public final void updateEntryPositions()
+        {
+        for ( int idx = 0; idx < entries.size; idx++ )
+            {
+            final MenuEntry entry = getEntry( idx );
+            entry.position.y = yOffset + FixedMath.toInt( idx * font.charHeight() * ySpacingFixed );
+            //#if TOUCH_SUPPORTED
+            entry.updateTouchable();
+            //#endif
+            }
         }
 
     //#ifdef TOUCH_SUPPORTED
@@ -50,18 +72,25 @@ public abstract class MenuBase extends MultiScreen implements TouchableHandler
         //#endif
         }
 
-    public void onPop()
+    public final void onPop()
         {
         //#if TOUCH_SUPPORTED
         removeTouchableAreas();
         //#endif
         }
 
-    public void onInitEverytime() throws Exception
+    public final void onInitEverytime() throws Exception
         {
+        beforeInitEverytime();
+
+        setOffsetToVerticallyCentered();
+        updateEntryPositions();
+
         //#if TOUCH_SUPPORTED
         addTouchableAreas();
         //#endif
+
+        afterInitEverytime();
         }
 
     public final void onControlTick() throws Exception
@@ -69,19 +98,19 @@ public abstract class MenuBase extends MultiScreen implements TouchableHandler
         final KeysHandler keys = system().keys;
         if ( keys.checkUpAndConsume() )
             {
-            updateSelectedEntry( selectedEntry - 1 );
+            updateSelectedEntry( selectedEntryIndex - 1 );
             }
         if ( keys.checkDownAndConsume() )
             {
-            updateSelectedEntry( selectedEntry + 1 );
+            updateSelectedEntry( selectedEntryIndex + 1 );
             }
         if ( keys.checkLeftSoftAndConsume() || keys.checkStickDownAndConsume() || keys.checkFireAndConsume() )
             {
-            onLeftSoftKey( getEntry( selectedEntry ) );
+            onLeftSoftKey( getEntry( selectedEntryIndex ) );
             }
         if ( keys.checkRightSoftAndConsume() )
             {
-            onRightSoftKey( getEntry( selectedEntry ) );
+            onRightSoftKey( getEntry( selectedEntryIndex ) );
             }
 
         super.onControlTick();
@@ -90,6 +119,14 @@ public abstract class MenuBase extends MultiScreen implements TouchableHandler
     // Protected Interface
 
     protected abstract void onSelected( final MenuEntry aSelectedEntry ) throws Exception;
+
+    protected void beforeInitEverytime() throws Exception
+        {
+        }
+
+    protected void afterInitEverytime() throws Exception
+        {
+        }
 
     protected void onLeftSoftKey( final MenuEntry aSelectedEntry ) throws Exception
         {
@@ -104,43 +141,48 @@ public abstract class MenuBase extends MultiScreen implements TouchableHandler
 
     protected final void removeAllEntries()
         {
-        for ( int idx = 0; idx < menuEntries.size; idx++ )
+        for ( int idx = 0; idx < entries.size; idx++ )
             {
-            removeScreen( (ScreenBase) menuEntries.get( idx ) );
+            removeScreen( (ScreenBase) entries.get( idx ) );
             }
-        menuEntries.clear();
+        entries.clear();
         }
 
     protected final MenuEntry addMenuEntry( final int aID, final String aText ) throws Exception
         {
         final int x = screen().width() / 2;
-        final int y = ( menuEntries.size + 2 ) * ( menuFontGen.charHeight() * 3 / 2 );
+        final int y = yOffset + font.charHeight() * FixedMath.toInt( entries.size * ySpacingFixed );
 
-        final MenuEntry newEntry = new MenuEntry( menuFontGen, aText, new Position( x, y ) );
+        final MenuEntry newEntry = new MenuEntry( font, aText, new Position( x, y ) );
         newEntry.id = aID;
         //#if TOUCH_SUPPORTED
         newEntry.touchable.associatedHandler = this;
         //#endif
         addScreen( newEntry );
-        menuEntries.add( newEntry );
+        entries.add( newEntry );
         return newEntry;
         }
 
     protected final void updateSelectedEntry()
         {
-        updateSelectedEntry( selectedEntry );
+        updateSelectedEntry( selectedEntryIndex );
         }
 
-    protected void updateSelectedEntry( final int aSelectedEntry )
+    protected final void updateSelectedEntry( final int aSelectedEntry )
         {
-        final int numberOfEntries = menuEntries.size;
-        selectedEntry = ( aSelectedEntry + numberOfEntries ) % numberOfEntries;
+        final int numberOfEntries = entries.size;
+        selectedEntryIndex = ( aSelectedEntry + numberOfEntries ) % numberOfEntries;
 
         for ( int idx = 0; idx < numberOfEntries; idx++ )
             {
             final MenuEntry menuEntry = getEntry( idx );
-            menuEntry.setSelected( idx == selectedEntry );
+            menuEntry.setSelected( idx == selectedEntryIndex );
             }
+        }
+
+    protected final MenuEntry getEntry( final int aIndex )
+        {
+        return (MenuEntry) entries.get( aIndex );
         }
 
     // Implementation
@@ -150,7 +192,7 @@ public abstract class MenuBase extends MultiScreen implements TouchableHandler
     private void addTouchableAreas()
         {
         final TouchHandler touch = touch();
-        final int numberOfMenuEntries = menuEntries.size;
+        final int numberOfMenuEntries = entries.size;
         for ( int idx = 0; idx < numberOfMenuEntries; idx++ )
             {
             touch.addLocalControl( getEntry( idx ).touchable );
@@ -160,7 +202,7 @@ public abstract class MenuBase extends MultiScreen implements TouchableHandler
     private void removeTouchableAreas()
         {
         final TouchHandler touch = touch();
-        final int numberOfMenuEntries = menuEntries.size;
+        final int numberOfMenuEntries = entries.size;
         for ( int idx = 0; idx < numberOfMenuEntries; idx++ )
             {
             touch.removeLocalControl( getEntry( idx ).touchable );
@@ -168,9 +210,4 @@ public abstract class MenuBase extends MultiScreen implements TouchableHandler
         }
 
     //#endif
-
-    private MenuEntry getEntry( final int aIndex )
-        {
-        return (MenuEntry) menuEntries.get( aIndex );
-        }
     }
