75c7d76197895a926d979a2da58da9829390af62
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-02-22 17:31:49 +0100

    Updated SoftkeysScreen implementation with code from VerticalSoftkeysScreen.

--- src/net/intensicode/screens/AutohideSoftkeysScreen.java
+++ src/net/intensicode/screens/AutohideSoftkeysScreen.java
@@ -1,6 +1,7 @@
 package net.intensicode.screens;
 
 import net.intensicode.graphics.FontGenerator;
+import net.intensicode.screens.SoftkeysScreen;
 
 public final class AutohideSoftkeysScreen extends SoftkeysScreen
     {
@@ -12,8 +13,8 @@ public final class AutohideSoftkeysScreen extends SoftkeysScreen
     public final void setSoftkeys( final String aLeftButton, final String aRightButton, final boolean aAutoHide )
         {
         setSoftkeys( aLeftButton, aRightButton );
-        myMakeVisibleFlag = true;
-        myAutoHide = aAutoHide;
+        resetAnimation();
+        myAutoHide = true;
         }
 
     // From ScreenBase
@@ -21,33 +22,42 @@ public final class AutohideSoftkeysScreen extends SoftkeysScreen
     public final void onControlTick()
         {
         super.onControlTick();
-        if ( myMakeVisibleFlag )
-            {
-            final int tps = system().timing.ticksPerSecond;
-            myVisibleTicks = tps * 3 / 2;
-            myHideTick = myHideTicks = tps * 2 / 3;
-            myMakeVisibleFlag = false;
-            }
-        else if ( myVisibleTicks > 0 )
-            {
-            myVisibleTicks--;
-            }
-        else if ( myHideTick > 0 && myAutoHide )
-                {
-                myHideTick--;
-                }
+        if ( myVisibleTicks > 0 ) myVisibleTicks--;
+        else if ( myHideTick > 0 && myAutoHide ) myHideTick--;
         }
 
-    public final void onDrawFrame()
+    // Protected API
+
+    protected void setPosition( final int aHorizontalPosition, final String aText )
         {
-        final int maxOffset = ( myOffsetY + myFontGen.charHeight() );
-        final int yOffset = myHideTicks == 0 ? 0 : maxOffset - myHideTick * maxOffset / myHideTicks;
-
-        myOffsetY -= yOffset;
-        super.onDrawFrame();
-        myOffsetY += yOffset;
+        final int xOffset = getAnimatedOffset( aText );
+        final int offset = aHorizontalPosition == 0 ? -xOffset : +xOffset;
+        super.setPosition( aHorizontalPosition + offset, aText );
         }
 
+    // Implementation
+
+    private void resetAnimation()
+        {
+        final int tps = timing().ticksPerSecond;
+        myVisibleTicks = tps * 3 / 2;
+        myHideTick = myHideTicks = tps * 2 / 3;
+        }
+
+    private int getAnimatedOffset( final String aText )
+        {
+        if ( !myAutoHide ) return 0;
+
+        final int maxOffset = getAlignWidth( aText );
+        if ( isCompletetlyHidden() ) return 0;
+
+        return maxOffset / 2 - myHideTick * maxOffset / myHideTicks / 2;
+        }
+
+    private boolean isCompletetlyHidden()
+        {
+        return myHideTicks == 0;
+        }
 
 
     private int myHideTick;
@@ -57,6 +67,4 @@ public final class AutohideSoftkeysScreen extends SoftkeysScreen
     private int myVisibleTicks;
 
     private boolean myAutoHide;
-
-    private boolean myMakeVisibleFlag;
     }
--- src/net/intensicode/screens/SoftkeysScreen.java
+++ src/net/intensicode/screens/SoftkeysScreen.java
@@ -2,6 +2,7 @@ package net.intensicode.screens;
 
 import net.intensicode.core.*;
 import net.intensicode.graphics.FontGenerator;
+import net.intensicode.screens.ScreenBase;
 import net.intensicode.util.*;
 
 public class SoftkeysScreen extends ScreenBase
@@ -9,213 +10,234 @@ public class SoftkeysScreen extends ScreenBase
     public SoftkeysScreen( final FontGenerator aFont )
         {
         myFontGen = aFont;
-        }
-
-    public final void setSoftkeys( final String aLeftButton, final String aRightButton )
-        {
         //#if TOUCH_SUPPORTED
-        myLeftChanged = hasButtonChanged( myLeftButton, aLeftButton );
-        myRightChanged = hasButtonChanged( myRightButton, aRightButton );
+        myLeftTouchRect.associatedKeyID = KeysHandler.LEFT_SOFT;
+        myRightTouchRect.associatedKeyID = KeysHandler.RIGHT_SOFT;
         //#endif
-
-        myLeftButton = aLeftButton;
-        myRightButton = aRightButton;
+        setPositionInPercent( 100 );
         }
 
-    public final void setInsets( final int aX, final int aY )
+    public final void setButtonImage( final ImageResource aButtonImage )
         {
-        myOffsetX = aX;
-        myOffsetY = aY;
+        myButtonImage = aButtonImage;
+        mySomethingChanged = true;
         }
 
-    public void placeImage( final ImageResource aImageResource, final int aX, final int aY )
+    public final void setSoftkeys( final String aLeftText, final String aRightText )
         {
-        myPlacedImage = aImageResource;
-        myImagePosition.x = aX;
-        myImagePosition.y = aY;
+        myLeftText = aLeftText;
+        myRightText = aRightText;
+        mySomethingChanged = true;
         }
 
-    private ImageResource myPlacedImage;
-
-    private final Position myImagePosition = new Position();
-
-    //#if TOUCH_SUPPORTED
-
-    public void setTouchableAreas( final Rectangle aLeft, final Rectangle aRight )
+    public final void setInsets( final int aHorizontalPixels, final int aVerticalPixels )
         {
-        myLeftTouchableRect = aLeft;
-        myRightTouchableRect = aRight;
+        myInsetX = aHorizontalPixels;
+        myInsetY = aVerticalPixels;
+        mySomethingChanged = true;
         }
 
-    private Rectangle myLeftTouchableRect;
+    public final void setPositionInPercent( final int aVerticalPositionInPercent )
+        {
+        myVerticalPositionMode = MODE_VERTICAL_POSITION_PERCENT;
+        myVerticalPositionValue = aVerticalPositionInPercent;
+        mySomethingChanged = true;
+        }
 
-    private Rectangle myRightTouchableRect;
-
-    //#endif
+    public final void setVerticalPosition( final int aVerticalPositionInPixels )
+        {
+        myVerticalPositionMode = MODE_VERTICAL_POSITION_ABSOLUTE;
+        myVerticalPositionValue = aVerticalPositionInPixels;
+        mySomethingChanged = true;
+        }
 
     // From ScreenBase
-
-    public final void onTop()
-        {
-        //#if TOUCH_SUPPORTED
-        updateLeftTouchButton();
-        updateRightTouchButton();
-        //#endif
-        }
-
-    public void onPop()
-        {
-        //#if TOUCH_SUPPORTED
-        removeLeftTouchButton();
-        removeRightTouchButton();
-        //#endif
-        }
 
     public void onControlTick()
         {
         //#if TOUCH_SUPPORTED
-        if ( myLeftChanged ) updateLeftTouchButton();
-        if ( myRightChanged ) updateRightTouchButton();
-        myLeftChanged = myRightChanged = false;
+        if ( mySomethingChanged ) updateTouchableAreas();
+        else tickTouchableAreas();
         //#endif
+        mySomethingChanged = false;
         }
 
     public void onDrawFrame()
         {
-        final DirectGraphics gc = graphics();
+        final DirectGraphics graphics = graphics();
 
-        if ( myPlacedImage != null )
+        if ( hasLeftText() )
             {
-            gc.drawImage( myPlacedImage, myImagePosition.x, myImagePosition.y );
+            setPosition( 0, myLeftText );
+            if ( myButtonImage != null )
+                {
+                graphics.drawImage( myButtonImage, myPosition.x, myPosition.y, ALIGN_TOP_LEFT );
+                }
+            blitTextString( myLeftText, ALIGN_TOP_LEFT );
             }
 
-        if ( myLeftButton != null )
+        if ( hasRightText() )
             {
-            myBlitPosition.x = myOffsetX;
-            myBlitPosition.y = screen().height() - myOffsetY;
-            myFontGen.blitString( gc, myLeftButton, myBlitPosition, FontGenerator.LEFT | FontGenerator.BOTTOM );
+            setPosition( screen().width(), myRightText );
+            if ( myButtonImage != null )
+                {
+                graphics.drawImage( myButtonImage, myPosition.x, myPosition.y, ALIGN_TOP_RIGHT );
+                }
+            blitTextString( myRightText, ALIGN_TOP_RIGHT );
             }
+        }
 
-        if ( myRightButton != null )
-            {
-            myBlitPosition.x = screen().width() - myOffsetX;
-            myBlitPosition.y = screen().height() - myOffsetY;
-            myFontGen.blitString( gc, myRightButton, myBlitPosition, FontGenerator.RIGHT | FontGenerator.BOTTOM );
-            }
+    // Protected API
+
+    protected void setPosition( final int aHorizontalPosition, final String aText )
+        {
+        final int height = getAlignHeight();
+        myPosition.x = aHorizontalPosition;
+        myPosition.y = calcVerticalPosition( height );
+        }
+
+    protected final int getAlignWidth( final String aText )
+        {
+        if ( myButtonImage != null ) return myButtonImage.getWidth();
+        return myFontGen.stringWidth( aText ) + myInsetX * 2;
+        }
+
+    protected final int getAlignHeight()
+        {
+        if ( myButtonImage != null ) return myButtonImage.getHeight();
+        return myFontGen.charHeight() + myInsetY * 2;
         }
 
     // Implementation
 
+    private boolean hasLeftText()
+        {
+        return myLeftText != null && myLeftText.length() > 0;
+        }
+
+    private boolean hasRightText()
+        {
+        return myRightText != null && myRightText.length() > 0;
+        }
+
+    private int getTextHeight( final String aText )
+        {
+        return myFontGen.charHeight();
+        }
+
+    private int calcVerticalPosition( final int aObjectHeight )
+        {
+        if ( myVerticalPositionMode == MODE_VERTICAL_POSITION_ABSOLUTE )
+            {
+            return myVerticalPositionValue;
+            }
+        final int availableHeight = screen().height() - aObjectHeight;
+        return availableHeight * myVerticalPositionValue / ONE_HUNDRED_PERCENT;
+        }
+
+    private void blitTextString( final String aText, final int aAlignment )
+        {
+        final int alignWidth = getAlignWidth( aText );
+        final int alignHeight = getAlignHeight();
+        final int x = myPosition.x;
+        final int y = myPosition.y;
+        final Position aligned = DirectGraphics.getAlignedPosition( x, y, alignWidth, alignHeight, aAlignment );
+        final int xOffset = getOffsetX( aText );
+        final int yOffset = getOffsetY();
+        final DirectGraphics graphics = graphics();
+        myFontGen.blitString( graphics,  aText, 0, aText.length(), aligned.x + xOffset, aligned.y + yOffset );
+        }
+
+    private int getOffsetX( final String aText )
+        {
+        if ( myButtonImage != null ) return ( myButtonImage.getWidth() - myFontGen.stringWidth( aText ) ) / 2;
+        else return myInsetX;
+        }
+
+    private int getOffsetY()
+        {
+        if ( myButtonImage != null ) return ( myButtonImage.getHeight() - myFontGen.charHeight() ) / 2;
+        else return myInsetX;
+        }
+
     //#if TOUCH_SUPPORTED
 
-    private boolean hasButtonChanged( final String aCurrentValue, final String aNewValue )
+    private void updateTouchableAreas()
         {
-        if ( aCurrentValue == null || aNewValue == null ) return aCurrentValue != aNewValue;
-        return !aCurrentValue.equals( aNewValue );
+        updateTouchableArea( myLeftTouchRect, myLeftText, 0 );
+        final int alignWidth = getAlignWidth( myRightText );
+        updateTouchableArea( myRightTouchRect, myRightText, screen().width() - alignWidth );
         }
 
-    private void updateLeftTouchButton()
+    private void tickTouchableAreas()
         {
-        if ( buttonValid( myLeftButton ) ) createLeftTouchButton();
-        else removeLeftTouchButton();
+        if ( hasLeftText() ) touch().addLocalControl( myLeftTouchRect );
+        if ( hasRightText() ) touch().addLocalControl( myRightTouchRect );
         }
 
-    private void updateRightTouchButton()
+    private void updateTouchableArea( final TouchableArea aTouchableArea, final String aText, final int aHorizontalPosition )
         {
-        if ( buttonValid( myRightButton ) ) createRightTouchButton();
-        else removeRightTouchButton();
-        }
-
-    private boolean buttonValid( final String aButtonTextOrNull )
-        {
-        return aButtonTextOrNull != null && aButtonTextOrNull.length() > 0;
-        }
-
-    // TODO: Clean up this touch button mess.. How!?
-
-    private void createLeftTouchButton()
-        {
-        final Rectangle rectangle = myLeftTouchRect.rectangle;
-        applyLabelBounds( rectangle, myLeftButton );
-        rectangle.x = myOffsetX;
-        rectangle.y = screen().height() - myOffsetY - rectangle.height;
-        applyOutsets( rectangle );
-
-        //#if TOUCH_SUPPORTED
-        if ( myLeftTouchableRect != null )
+        if ( aText == null || aText.length() == 0 )
             {
-            rectangle.setTo( myLeftTouchableRect );
+            touch().removeLocalControl( aTouchableArea );
             }
-        //#endif
-
-        myLeftTouchRect.associatedKeyID = KeysHandler.LEFT_SOFT;
-        system().touch.addLocalControl( myLeftTouchRect );
-        }
-
-    private void removeLeftTouchButton()
-        {
-        system().touch.removeLocalControl( myLeftTouchRect );
-        }
-
-    private void createRightTouchButton()
-        {
-        final Rectangle rectangle = myRightTouchRect.rectangle;
-        applyLabelBounds( rectangle, myRightButton );
-        rectangle.x = screen().width() - myOffsetX - rectangle.width;
-        rectangle.y = screen().height() - myOffsetY - rectangle.height;
-        applyOutsets( rectangle );
-
-        //#if TOUCH_SUPPORTED
-        if ( myRightTouchableRect != null )
+        else
             {
-            rectangle.setTo( myRightTouchableRect );
+            setPosition( aHorizontalPosition, aText );
+            aTouchableArea.rectangle.x = myPosition.x;
+            aTouchableArea.rectangle.y = myPosition.y;
+            aTouchableArea.rectangle.width = getAlignWidth( aText );
+            aTouchableArea.rectangle.height = getAlignHeight();
+            touch().addLocalControl( aTouchableArea );
             }
-        //#endif
-
-        myRightTouchRect.associatedKeyID = KeysHandler.RIGHT_SOFT;
-        system().touch.addLocalControl( myRightTouchRect );
         }
 
-    private void removeRightTouchButton()
+    private void removeTouchableAreas()
         {
-        system().touch.removeLocalControl( myRightTouchRect );
-        }
-
-    private void applyLabelBounds( final Rectangle aRectangle, final String aLabel )
-        {
-        aRectangle.width = myFontGen.stringWidth( aLabel );
-        aRectangle.height = myFontGen.charHeight();
-        }
-
-    private void applyOutsets( final Rectangle aRectangle )
-        {
-        aRectangle.applyOutsets( aRectangle.height / 2 );
+        touch().removeLocalControl( myLeftTouchRect );
+        touch().removeLocalControl( myRightTouchRect );
         }
 
     //#endif
 
 
-    protected int myOffsetX = 4;
+    private int myInsetX = 4;
 
-    protected int myOffsetY = 2;
+    private int myInsetY = 2;
 
-    protected String myLeftButton;
+    private String myLeftText;
 
-    protected String myRightButton;
+    private String myRightText;
 
-    protected final FontGenerator myFontGen;
+    private ImageResource myButtonImage;
 
-    private final Position myBlitPosition = new Position();
+    private int myVerticalPositionMode;
+
+    private int myVerticalPositionValue;
+
+    private boolean mySomethingChanged;
+
+
+    private final FontGenerator myFontGen;
+
+    private final Position myPosition = new Position();
 
     //#if TOUCH_SUPPORTED
-
-    private boolean myLeftChanged;
-
-    private boolean myRightChanged;
 
     private final TouchableArea myLeftTouchRect = new TouchableArea();
 
     private final TouchableArea myRightTouchRect = new TouchableArea();
 
     //#endif
+
+
+    private static final int ONE_HUNDRED_PERCENT = 100;
+
+    private static final int MODE_VERTICAL_POSITION_PERCENT = 0;
+
+    private static final int MODE_VERTICAL_POSITION_ABSOLUTE = 1;
+
+    private static final int ALIGN_TOP_LEFT = DirectGraphics.ALIGN_LEFT;
+
+    private static final int ALIGN_TOP_RIGHT = DirectGraphics.ALIGN_RIGHT;
     }
