b006fa0ad575818d6138c39ac1ecbcdc045ac9dc
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-03-02 13:47:14 +0100

    Turned TouchEvent into an abstract class and implemented equals and toString.

--- src/net/intensicode/core/TouchEvent.java
+++ src/net/intensicode/core/TouchEvent.java
@@ -2,15 +2,46 @@
 
 package net.intensicode.core;
 
-public interface TouchEvent
+public abstract class TouchEvent
     {
-    boolean isPress();
+    public abstract boolean isPress();
 
-    boolean isSwipe();
+    public abstract boolean isSwipe();
 
-    boolean isRelease();
+    public abstract boolean isRelease();
 
-    int getX();
+    public abstract int getX();
 
-    int getY();
+    public abstract int getY();
+
+    // From Object
+
+    public final boolean equals( final Object aObject )
+        {
+        if ( !( aObject instanceof TouchEvent ) ) return false;
+        final TouchEvent that = (TouchEvent) aObject;
+        if ( this.isPress() != that.isPress() ) return false;
+        if ( this.isSwipe() != that.isSwipe() ) return false;
+        if ( this.isRelease() != that.isRelease() ) return false;
+        if ( this.getX() != that.getX() ) return false;
+        if ( this.getY() != that.getY() ) return false;
+        return true;
+        }
+
+    public String toString()
+        {
+        final StringBuffer buffer = new StringBuffer();
+        buffer.append( "TouchEvent(" );
+        buffer.append( isPress() );
+        buffer.append( ',' );
+        buffer.append( isSwipe() );
+        buffer.append( ',' );
+        buffer.append( isRelease() );
+        buffer.append( ',' );
+        buffer.append( getX() );
+        buffer.append( ',' );
+        buffer.append( getY() );
+        buffer.append( ")" );
+        return buffer.toString();
+        }
     }
