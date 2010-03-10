ceae2f7f9470c984f7a13f23b812115deee8f92b
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-03-10 08:10:31 +0100

    Reworked AnalogController to provide per-direction delta.
    Additional 'max' and 'accumulationTicks' information should help
    determine the type of movement. Slow versus fast, etc.
    
    An additional 'silenceBeforeUpdateInMillis' can be set to avoid
    generating events while the user still moves the trackball.
    
    The 'silenceTimeoutInMillis' can be used to avoid stalling events for
    too long.

--- src/net/intensicode/core/AnalogController.java
+++ src/net/intensicode/core/AnalogController.java
@@ -1,9 +1,13 @@
 package net.intensicode.core;
 
-import net.intensicode.util.FixedMath;
-
 public abstract class AnalogController
     {
+    public static final int DEFAULT_SILENCE_IN_MILLIS = 10;
+
+    public static final int DEFAULT_SILENCE_TIMEOUT_IN_MILLIS = 25;
+
+    public static final int DEFAULT_IGNORE_FACTOR = 2;
+
     //#if SENSORS
 
     public boolean useOrientationSensor;
@@ -12,14 +16,64 @@ public abstract class AnalogController
 
     //#endif
 
-    public int xDeltaFixed;
+    public int silenceBeforeUpdateInMillis = DEFAULT_SILENCE_IN_MILLIS;
 
-    public int yDeltaFixed;
+    public int silenceTimeoutInMillis = DEFAULT_SILENCE_TIMEOUT_IN_MILLIS;
 
-    public int xSensitivityFixed = FixedMath.FIXED_1;
+    public int ignoreFactor = DEFAULT_IGNORE_FACTOR;
 
-    public int ySensitivityFixed = FixedMath.FIXED_1;
+    public boolean autoClear;
 
+    public int leftDelta;
+
+    public int rightDelta;
+
+    public int upDelta;
+
+    public int downDelta;
+
+    public int leftMax;
+
+    public int rightMax;
+
+    public int upMax;
+
+    public int downMax;
+
+    public int accumulationTicks;
+
+
+    public final int getDeltaX()
+        {
+        return rightDelta - leftDelta;
+        }
+
+    public final int getDeltaY()
+        {
+        return downDelta - upDelta;
+        }
+
+    public final int getMaxInAnyDirection()
+        {
+        return Math.max( leftMax, Math.max( rightMax, Math.max( upMax, downMax ) ) );
+        }
+
+    public final boolean shouldIgnoreDeltaX()
+        {
+        return Math.abs( getDeltaY() ) > Math.abs( getDeltaX() ) * ignoreFactor;
+        }
+
+    public final boolean shouldIgnoreDeltaY()
+        {
+        return Math.abs( getDeltaX() ) > Math.abs( getDeltaY() ) * ignoreFactor;
+        }
+
+    public final void clearDeltaValues()
+        {
+        leftDelta = rightDelta = upDelta = downDelta = 0;
+        leftMax = rightMax = upMax = downMax = 0;
+        accumulationTicks = 0;
+        }
 
     public final void onControlTick()
         {
@@ -27,8 +81,9 @@ public abstract class AnalogController
         if ( useOrientationSensor ) mapOrientationToMovement();
         if ( useAccelerationSensor ) mapAccelerationToMovement();
         //#endif
+
         if ( hasNewData() ) updateDeltaValues();
-        else clearDeltaValues();
+        else if ( autoClear ) clearDeltaValues();
         }
 
     // Protected Abstract API
@@ -44,11 +99,4 @@ public abstract class AnalogController
     protected abstract boolean hasNewData();
 
     protected abstract void updateDeltaValues();
-
-    // Implementation
-
-    private void clearDeltaValues()
-        {
-        xDeltaFixed = yDeltaFixed = 0;
-        }
     }
