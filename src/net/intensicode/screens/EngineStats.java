9e9b0a404c50bc9d984280db74f15dd221b7d57e
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-03-11 13:52:37 +0100

    Made EngineStats more compact.

--- src/net/intensicode/screens/EngineStats.java
+++ src/net/intensicode/screens/EngineStats.java
@@ -31,28 +31,28 @@ public final class EngineStats extends ScreenBase
         final int width = screen().width();
 
         graphics.setColorARGB32( 0x80000000 );
-        graphics.fillRect( width - font.maxCharWidth() * 7, 0, font.maxCharWidth() * 7, font.charHeight() * 4 );
+        graphics.fillRect( width - font.maxCharWidth() * 6, 0, font.maxCharWidth() * 6, font.charHeight() * 3 );
 
-        myBlitPos.y = font.charHeight();
+        myBlitPos.y = font.charHeight() / 2;
 
-        myBlitPos.x = width - font.maxCharWidth() * 4;
+        myBlitPos.x = width - font.maxCharWidth() * 4 + font.maxCharWidth() / 2;
         font.blitNumber( graphics, myBlitPos, timing().measuredFramesPerSecond, FontGenerator.RIGHT );
 
-        myBlitPos.x = width - font.maxCharWidth() * 4;
+        myBlitPos.x = width - font.maxCharWidth() * 4 + font.maxCharWidth() / 2;
         font.blitChar( graphics, myBlitPos.x, myBlitPos.y, '/' );
 
-        myBlitPos.x = width - font.maxCharWidth();
+        myBlitPos.x = width - font.maxCharWidth() + font.maxCharWidth() / 2;
         font.blitNumber( graphics, myBlitPos, timing().maxFramesPerSecond, FontGenerator.RIGHT );
 
-        myBlitPos.y = font.charHeight() * 2;
+        myBlitPos.y = font.charHeight() * 3 / 2;
 
-        myBlitPos.x = width - font.maxCharWidth() * 4;
+        myBlitPos.x = width - font.maxCharWidth() * 4 + font.maxCharWidth() / 2;
         font.blitNumber( graphics, myBlitPos, timing().measuredTicksPerSecond, FontGenerator.RIGHT );
 
-        myBlitPos.x = width - font.maxCharWidth() * 4;
+        myBlitPos.x = width - font.maxCharWidth() * 4 + font.maxCharWidth() / 2;
         font.blitChar( graphics, myBlitPos.x, myBlitPos.y, '/' );
 
-        myBlitPos.x = width - font.maxCharWidth();
+        myBlitPos.x = width - font.maxCharWidth() + font.maxCharWidth() / 2;
         font.blitNumber( graphics, myBlitPos, timing().ticksPerSecond, FontGenerator.RIGHT );
         }
 
