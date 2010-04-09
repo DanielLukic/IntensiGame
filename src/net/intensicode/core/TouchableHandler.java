c7e6005c9a6b0d726f50589eb433218425a6f35e
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-04-09 09:46:05 +0200

    Fixed TouchableHandler preprocessing to use #condition TOUCH.

--- src/net/intensicode/core/TouchableHandler.java
+++ src/net/intensicode/core/TouchableHandler.java
@@ -1,10 +1,10 @@
+//#condition TOUCH
+
 package net.intensicode.core;
 
 public interface TouchableHandler
     {
-    //#ifdef TOUCH
     void onPressed( Object aTouchable );
 
     void onReleased( Object aTouchable );
-    //#endif
     }
