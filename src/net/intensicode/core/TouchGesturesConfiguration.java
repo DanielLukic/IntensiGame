3fd43d6dffab04f5497fd55226788c3224fd2b74
Daniel Lukic <daniel.lukic@berlinfactor.com>
2010-04-13 17:05:22 +0200

    Added sensitivity presets with helper method to TouchGesturesConfiguration and TouchSliderConfiguration.

--- src/net/intensicode/core/TouchGesturesConfiguration.java
+++ src/net/intensicode/core/TouchGesturesConfiguration.java
@@ -2,10 +2,43 @@
 
 package net.intensicode.core;
 
-import net.intensicode.util.FixedMath;
+import net.intensicode.util.*;
 
 public final class TouchGesturesConfiguration
     {
+    public static final int SENSITIVITY_LOW = 1;
+
+    public static final int SENSITIVITY_MEDIUM = 2;
+
+    public static final int SENSITIVITY_HIGH = 3;
+
+    public void setSensitivityPreset( final int aSensitivityId )
+        {
+        Log.debug( "TouchGesturesConfiguration#setSensitivityPreset {}", aSensitivityId );
+        switch ( aSensitivityId )
+            {
+            default:
+            case SENSITIVITY_LOW:
+                breakTimeThresholdInMillis = 80;
+                samePositionThresholdInPixels = 12;
+                strokeThresholdInPixels = 6;
+                directionIgnoreFactorFixed = FixedMath.FIXED_1 + FixedMath.FIXED_0_5 + FixedMath.FIXED_0_25 + FixedMath.FIXED_0_1;
+                break;
+            case SENSITIVITY_MEDIUM:
+                breakTimeThresholdInMillis = 60;
+                samePositionThresholdInPixels = 10;
+                strokeThresholdInPixels = 5;
+                directionIgnoreFactorFixed = FixedMath.FIXED_1 + FixedMath.FIXED_0_5 + FixedMath.FIXED_0_25;
+                break;
+            case SENSITIVITY_HIGH:
+                breakTimeThresholdInMillis = 40;
+                samePositionThresholdInPixels = 8;
+                strokeThresholdInPixels = 4;
+                directionIgnoreFactorFixed = FixedMath.FIXED_1 + FixedMath.FIXED_0_5 + FixedMath.FIXED_0_1;
+                break;
+            }
+        }
+
     public int breakTimeThresholdInMillis = 80;
 
     public int samePositionThresholdInPixels = 12;
--- src/net/intensicode/core/TouchSliderConfiguration.java
+++ src/net/intensicode/core/TouchSliderConfiguration.java
@@ -2,8 +2,49 @@
 
 package net.intensicode.core;
 
+import net.intensicode.util.*;
+
 public final class TouchSliderConfiguration
     {
+    public static final int SENSITIVITY_LOW = 1;
+
+    public static final int SENSITIVITY_MEDIUM = 2;
+
+    public static final int SENSITIVITY_HIGH = 3;
+
+    public void setSensitivityPreset( final int aSensitivityId )
+        {
+        Log.debug( "TouchSliderConfiguration#setSensitivityPreset {}", aSensitivityId );
+        switch ( aSensitivityId )
+            {
+            default:
+            case SENSITIVITY_LOW:
+                slideStartThresholdInMillis = 25;
+                slideStartThresholdInPixels = 20;
+                slideMoveThresholdInPixels = 10;
+                newSlideStartThresholdInMillis = 50;
+                initialStepThresholdInPixels = 10;
+                additionalStepThresholdInPixels = 100;
+                break;
+            case SENSITIVITY_MEDIUM:
+                slideStartThresholdInMillis = 25;
+                slideStartThresholdInPixels = 15;
+                slideMoveThresholdInPixels = 8;
+                newSlideStartThresholdInMillis = 40;
+                initialStepThresholdInPixels = 8;
+                additionalStepThresholdInPixels = 50;
+                break;
+            case SENSITIVITY_HIGH:
+                slideStartThresholdInMillis = 25;
+                slideStartThresholdInPixels = 10;
+                slideMoveThresholdInPixels = 6;
+                newSlideStartThresholdInMillis = 30;
+                initialStepThresholdInPixels = 6;
+                additionalStepThresholdInPixels = 30;
+                break;
+            }
+        }
+
     public int slideStartThresholdInMillis = 25;
 
     public int slideStartThresholdInPixels = 20;
