998c19d4db853a2670c74d8afbf5e3ce0d125e9b
Daniel Lukic <daniel.lukic@berlinfactor.com>
2011-02-27 14:05:01 +0100

    Added NetworkIO#isOnline method.

--- src/net/intensicode/core/NetworkIO.java
+++ src/net/intensicode/core/NetworkIO.java
@@ -2,6 +2,8 @@ package net.intensicode.core;
 
 public interface NetworkIO
     {
+    boolean isOnline();
+
     void sendAndReceive( String aURL, byte[] aBody, NetworkCallback aCallback );
 
     void process( NetworkRequest aRequest, NetworkCallback aNetworkCallback );
