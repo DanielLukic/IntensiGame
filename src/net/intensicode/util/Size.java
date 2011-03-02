dad043b3a31a6bf99c3a526adefa108d35f82bf0
Daniel Lukic <daniel.lukic@berlinfactor.com>
2011-03-03 00:53:26 +0100

    Added equals and toString for Size class.

--- src/net/intensicode/util/Size.java
+++ src/net/intensicode/util/Size.java
@@ -27,4 +27,22 @@ public final class Size
         width = aSize.width;
         height = aSize.height;
         }
+
+    // From Object
+
+    public final boolean equals( final Object aObject )
+        {
+        if ( !( aObject instanceof Size ) ) return false;
+        final Size that = (Size) aObject;
+        return this.width == that.width && this.height == that.height;
+        }
+
+    public final String toString()
+        {
+        final StringBuffer buffer = new StringBuffer();
+        buffer.append( width );
+        buffer.append( 'x' );
+        buffer.append( height );
+        return buffer.toString();
+        }
     }
